46f0c4e7ac86e9c618905073b4a087cd41790f35
Danila Mororozv <morozov.danila96@gmail.com>
2020-12-21 15:38:27 +0300

    add flyway, docker

--- backend_module/src/main/java/com/natlex/backend_module/BackendModuleApplication.java
+++ backend_module/src/main/java/com/natlex/backend_module/BackendModuleApplication.java
@@ -1,13 +1,16 @@
 package com.natlex.backend_module;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class BackendModuleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendModuleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendModuleApplication.class, args);
+    }
 
 }
